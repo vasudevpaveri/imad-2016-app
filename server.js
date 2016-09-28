599dbe2f283797c6016e668cae0b2f77df36f737
vasudevpaveri <vpaveri96@gmail.com>
2016-09-28 16:29:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){res,send('Article-one requested served here');
+app.get('/article-one',function(req,res){res,send('Article-one requestes served here');
     
 });
-app.get('/article-two',function(req,res){res,send('Article-two requested served here');
+app.get('/article-two',function(req,res){res,send('Article-two requestes served here');
     
 });
-app.get('/article-three',function(req,res){res,send('Article-three requested served here');
+app.get('/article-three',function(req,res){res,send('Article-three requestes served here');
     
 });
 app.get('/ui/style.css', function (req, res) {
