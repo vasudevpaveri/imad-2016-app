cd08e5ebe30b77eb322d37f45ff5a64e9c5d471a
vasudevpaveri <vpaveri96@gmail.com>
2016-09-29 17:02:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one', function(req, res) {
     });
 
 app.get('/article-two', function(req, res) {
-    res.send('Article-two requestes served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     });
 
 app.get('/article-three', function(req, res) {
